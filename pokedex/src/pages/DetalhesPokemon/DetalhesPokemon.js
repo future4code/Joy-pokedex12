afdd9416e990c299ee4dcc45854cd37f0eabdcf2
AlinePaulinoMartins <alinepama15@gmail.com>
2022-02-03 21:56:26 -0300

    implementei react-Router

---
+++ pokedex/src/pages/DetalhesPokemon/DetalhesPokemon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const DetalhesPokemon = () => {
+    return (
+        <div>
+            <h1>Detalhes Pokemon</h1>
+        </div>
+    )
+}
+export default DetalhesPokemon
