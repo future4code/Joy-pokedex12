cc3ae19a699e4cdd674cf0123e5800680fbf85c0
BrunaAlv <bru_trakinas@hotmail.com>
2022-02-08 23:47:24 -0300

    Lista/Paginação

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Pokedex from './pages/Pokedex/Pokedex';
 import DetalhesPokemon from './pages/DetalhesPokemon/DetalhesPokemon';
-
+import PokemonList from "./PokemonList";
+import axios from 'axios'
+import Pagination from "./Pagination";
 
 function App() {
+  const [pokemon, setPokemon] = useState ([]);
+  const [currentPageURL, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageURL, setNextPageUrl] = useState();
+  const [prevPageURL, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    setLoading(true)
+    let cancel
+    axios
+    .get(currentPageURL, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
+    .then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p =>p.name))
+    })
+
+    return() => cancel();
+
+    }, [currentPageURL]);
+
+    function gotoNextPage(){
+      setCurrentPageUrl(nextPageURL)
+    }
+
+    function gotoPrevPage(){
+      setCurrentPageUrl(prevPageURL)
+    }
+  if (loading) return "Loading..."
+
+
   return (
-    <BrowserRouter>
+    <><BrowserRouter>
       <Switch>
           <Route exact path={"/"}>
             <Home />
@@ -20,6 +56,13 @@ function App() {
         </Route>
       </Switch>
     </BrowserRouter>
+
+        <PokemonList pokemon = {pokemon} />
+        <Pagination 
+          gotoNextPage={nextPageURL ? gotoNextPage :  null}
+          gotoPrevPage={prevPageURL ? gotoPrevPage : null}
+        /> 
+    </>
   );
 }
 
